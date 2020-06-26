adbedd7a3b1e60d7dafcb37a50c4a9f2a585fa2b
Edward Li <edward@barnabasroboics.com>
2020-06-26 16:24:45 -0700

    adding build scripts

---
+++ build_exe.bat
@@ -0,0 +1,21 @@
+: CD to ardublock-all directory
+cd ardublock-all
+
+: Create clean maven package
+call mvn clean
+call mvn package
+
+: CD back to ardublock-standalone directory
+cd..
+
+: Move new ardubock.jar file to the bundler
+move /y %CD%\ardublock-all\target\ardublock-all.jar %CD%\bundler\target_win
+
+: CD to bundler directory
+cd bundler
+
+: Build package
+call jpackage --input target_win/ --name BarnabasArdublock --main-jar ardublock-all.jar --main-class com.ardublock.Main --type exe --java-options '--enable-preview' --icon "barnabas_logo.ico" --win-shortcut
+
+: Pause to allow user to see terminal feedback
+pause
---
+++ build_pkg.sh
@@ -0,0 +1,21 @@
+: CD to ardublock-all directory
+cd ardublock-all
+
+: Create clean maven package
+call mvn clean
+call mvn package
+
+: CD back to ardublock-standalone directory
+cd..
+
+: Move new ardubock.jar file to the bundler
+mv /y $(dirname "$0")/ardublock-all/target/ardublock-all.jar $(dirname "$0")/bundler/target_mac
+
+: CD to bundler directory
+cd bundler
+
+: Build package
+jpackage --input target_mac/ --name BarnabasArdublock --main-jar ardublock-all.jar --main-class com.ardublock.Main --type pkg --java-options '--enable-preview' --icon "barnabas_logo.icns"
+
+: Pause to allow user to see terminal feedback
+pause
